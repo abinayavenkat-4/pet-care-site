9627a7bd71037e16688f4906f1d67628947d9ab7
abinayavenkat-4 <abinayamanivaannan04@gmail.com>
2024-12-12 11:30:38 +0530

    Updated index.html, script.js, style.css, and added contact image

--- script.js
+++ script.js
@@ -1,35 +1,66 @@
 
 
 
-let loginForm = document.querySelector('.header .login-form');
-  document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
+// let loginForm = document.querySelector('.header .login-form');
+//   document.querySelector('#login-btn').onclick = () =>{
+//     loginForm.classList.toggle('active');
+//     navbar.classList.remove('active');
+// }
 
-let navbar = document.querySelector('.header .navbar');
-  document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
-}
+// let navbar = document.querySelector('.header .navbar');
+//   document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+//     loginForm.classList.remove('active');
+// }
 
-window.onscroll = () =>{
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
+// window.onscroll = () =>{
+//     loginForm.classList.remove('active');
+//     navbar.classList.remove('active');
 
-    if(window.scrollY > 0){
-       document.querySelector('.header').classList.add('active');
-        }  
-        else{
-         document.querySelector('.header').classList.remove('active');
-         }
-}
- window.onload = () =>{
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-         }  
-        else{
-        document.querySelector('.header').classList.remove('active');
-            }
-}
+//     if(window.scrollY > 0){
+//        document.querySelector('.header').classList.add('active');
+//         }  
+//         else{
+//          document.querySelector('.header').classList.remove('active');
+//          }
+// }
+//  window.onload = () =>{
+//     if(window.scrollY > 0){
+//         document.querySelector('.header').classList.add('active');
+//          }  
+//         else{
+//         document.querySelector('.header').classList.remove('active');
+//             }
+// }
     
+let loginForm = document.querySelector('.header .login-form');
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+  navbar.classList.remove('active');
+};
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+  loginForm.classList.remove('active');
+};
+
+window.onscroll = () => {
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+window.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
